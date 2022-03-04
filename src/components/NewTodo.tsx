fd7adc1b94a1c7613fcc6ad03c3542a3bf905f59
Amrita Matta <matta.amrita@gmail.com>
2022-03-04 10:45:02 +0200

    added use ref & displayed on console

--- src/components/NewTodo.tsx
+++ src/components/NewTodo.tsx
@@ -1,16 +1,19 @@
-import React from "react";
+import React, { useRef } from "react";
 
 const NewTodo: React.FC = () => {
   const todoSubmitHandler = (event: React.FormEvent) => {
     event.preventDefault();
+    const enteredText = textInputRef.current!.value;
+    console.log(enteredText);
   };
+  const textInputRef = useRef<HTMLInputElement>(null);
   return (
     <form onSubmit={todoSubmitHandler}>
       <div>
         <label htmlFor="todo-txt">Todo Text</label>
-        <input type="text" id="todo-text" />
+        <input type="text" id="todo-text" ref={textInputRef} />
       </div>
-      <button type="submit"></button>
+      <button type="submit">Add ToDo</button>
     </form>
   );
 };
